a7874506fd8395ba808eab32e09b9ba0cb357ac8
Lonmee <Lonmee@126.com>
2021-11-26 16:15:13 +0800

    x

---
+++ nodejs-assets/nodejs-project/test.js
@@ -0,0 +1,20 @@
+const Server = require('ssb-server');
+const config = require('ssb-config');
+const fs = require('fs');
+const path = require('path');
+
+// add plugins
+Server.use(require('ssb-master'))
+  .use(require('ssb-gossip'))
+  .use(require('ssb-replicate'))
+  .use(require('ssb-backlinks'));
+
+const server = Server(config);
+
+// save an updated list of methods this server has made public
+// in a location that ssb-client will know to check
+const manifest = server.getManifest();
+fs.writeFileSync(
+  path.join(config.path, 'manifest.json'), // ~/.ssb/manifest.json
+  JSON.stringify(manifest),
+);
