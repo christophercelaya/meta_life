7c492f250854b0bd7523b0c4a1c5c8ae510b1295
Lonmee <Lonmee@126.com>
2021-12-20 18:42:22 +0800

    * doing: ssb-svr ops @ stream

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ import {persistor, store} from './src/store/configureStore';
 import {PersistGate} from 'redux-persist/integration/react';
 import nodejs from 'nodejs-mobile-react-native';
 
-nodejs.start('loader.js');
+setTimeout(() => {
+  nodejs.start('loader.js');
+}, 1);
 
 const WrappedApp = () => {
   return (
