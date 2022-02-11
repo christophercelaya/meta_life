69a726fc9f42136d3ff73b6dfa0835dd5b6185c0
Lonmee <Lonmee@126.com>
2022-02-11 18:17:08 +0800

    * refine script

--- tools/backend/move-to-nodejs-assets.sh
+++ tools/backend/move-to-nodejs-assets.sh
@@ -15,7 +15,6 @@ function onFailure() {
 }
 
 mkdir -p ./nodejs-assets/nodejs-project;
-mv ./nodejs-assets/nodejs-project/README.md ./NODEJS-ASSETS-README.md;
 rm -rf ./nodejs-assets/nodejs-project;
 if [ -f ./nodejs-assets/BUILD_NATIVE_MODULES.txt ]
 then
@@ -35,4 +34,3 @@ mv ./loader.mobile.js ./loader.js;
 rm ./*.js.map;
 rm ./plugins/*.js.map;
 cd ../..;
-mv ./NODEJS-ASSETS-README.md ./nodejs-assets/nodejs-project/README.md;
